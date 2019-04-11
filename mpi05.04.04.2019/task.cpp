46afb6a13124bc669d1430eafbe62da0af5cd1bc
QuickSilver <avataraang399@gmail.com>
2019-04-11 14:06:11 +1000

    Update mpi05

--- mpi05.04.04.2019/task.cpp
+++ mpi05.04.04.2019/task.cpp
@@ -18,22 +18,22 @@ int main (int argc, char* argv[])
 
   switch (rank) {
     case 1: MPI_Send(&ibuf, 1, MPI_INT, 0, 5, MPI_COMM_WORLD); break;
-    case 2: MPI_Send(&fbuf, 1, MPI_FLOAT, 0, 5, MPI_COMM_WORLD); break;
+    case 2: MPI_Send(&fbuf, 1, MPI_FLOAT, 0, 6, MPI_COMM_WORLD); break;
     case 0:
-      MPI_Probe(MPI_ANY_SOURCE, 5, MPI_COMM_WORLD, &status);
-      if (status.MPI_SOURCE == 1) {
+      MPI_Probe(MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+      if (status.MPI_TAG == 5) {
         MPI_Recv(&ibuf, 1, MPI_INT, 1, 5, MPI_COMM_WORLD, &status);
-        MPI_Recv(&fbuf, 1, MPI_FLOAT, 2, 5, MPI_COMM_WORLD, &status);
+        MPI_Recv(&fbuf, 1, MPI_FLOAT, 2, 6, MPI_COMM_WORLD, &status);
         std::cout << "Process 0 recv "
-                  << ibuf << " from process 1, "
-                  << fbuf << " from process 2"
+                  << ibuf << " from tag 5, "
+                  << fbuf << " from tag 6"
                   << std::endl;
-      } else if (status.MPI_SOURCE == 2) {
-        MPI_Recv(&fbuf, 1, MPI_FLOAT, 2, 5, MPI_COMM_WORLD, &status);
+      } else if (status.MPI_TAG == 6) {
+        MPI_Recv(&fbuf, 1, MPI_FLOAT, 2, 6, MPI_COMM_WORLD, &status);
         MPI_Recv(&ibuf, 1, MPI_INT, 1, 5, MPI_COMM_WORLD, &status);
         std::cout << "Process 0 recv "
-                  << fbuf << " from process 2, "
-                  << ibuf << " from process 1"
+                  << fbuf << " from tag 6 "
+                  << ibuf << " from tag 5"
                   << std::endl;
       }
       break;
